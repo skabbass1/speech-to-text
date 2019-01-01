86f8c7baaa028cf3c9c852195cb946667f4dd95c
sabbas <syed.k.abbas1988@gmail.com>
2019-01-01 10:54:50 -0600

    rename transcriber to recognizer

--- tests/test_recognizer.py
+++ tests/test_recognizer.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 
 import pytest
 
-import transcriber
+import recognizer
 
 def test_build_request_payload(audio_file_uri):
     """
     it correctly builds the request payload
     """
-    got = transcriber._build_request_payload(audio_file_uri)
+    got = recognizer._build_request_payload(audio_file_uri)
     expected = {
         'config': {'languageCode': 'en-US'},
         'audio': {'uri': audio_file_uri}
@@ -25,7 +25,7 @@ def test_submit_for_recognition(audio_file_uri, monkeypatch):
         return _MockResponse(status_code=200, json_content={'result': 'OK'})
 
     monkeypatch.setattr(requests, 'post', mock_post)
-    got = transcriber.submit_for_recognition(
+    got = recognizer.submit_for_recognition(
             audio_file_uri,
             api_key='hdtge'
        )
@@ -42,8 +42,8 @@ def test_submit_for_recognition_raises(audio_file_uri, monkeypatch):
 
     monkeypatch.setattr(requests, 'post', mock_post)
 
-    with pytest.raises(transcriber.RecognizerException):
-        got = transcriber.submit_for_recognition(
+    with pytest.raises(recognizer.RecognizerException):
+        got = recognizer.submit_for_recognition(
                 audio_file_uri,
                 api_key='hdtge'
            )
