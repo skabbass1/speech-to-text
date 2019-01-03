b65cdd8a93f520acf524d26f54776b47587f88af
sabbas <syed.k.abbas1988@gmail.com>
2019-01-03 06:39:48 -0600

    add a flaky test for cloud store

--- cloud_store.py
+++ cloud_store.py
@@ -4,7 +4,8 @@ from google.cloud import storage
 
 def upload_audio_file_to_bucket(file_path, bucket_name):
     bucket = _get_bucket(bucket_name)
-    bucket.blob(pathlib.Path(file_path).name).upload_from_filename(file_path)
+    blob = bucket.blob(pathlib.Path(file_path).name)
+    blob.upload_from_filename(file_path)
 
 def _get_bucket(bucket_name):
     client = storage.Client()
--- tests/test_cloud_store.py
+++ tests/test_cloud_store.py
@@ -3,13 +3,9 @@ from unittest import mock
 import pytest
 import cloud_store
 
+# TODO: This should be a proper integration test. Currently google
+# does not provide a simulated object store for local testing.
 def test_upload_audio_file_to_bucket(monkeypatch):
     from google.cloud import storage
-
     monkeypatch.setattr(storage.Client, 'get_bucket', mock.MagicMock())
-    breakpoint()
-    cloud_store.upload_audio_file_to_bucket('', '')
-    pass
-
-
-
+    cloud_store.upload_audio_file_to_bucket('/some/file', 'some_bucket')
